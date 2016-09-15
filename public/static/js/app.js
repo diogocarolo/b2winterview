61a0a7fb493c04a299d2f35baaf65438e0103380
Diogo Carolo <diogocarolo@yahoo.com.br>
2016-09-15 10:35:42 -0300

    change bottle for flask

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', ['infinite-scroll']);
 
 app.controller('tweets', ['$scope', '$http', function ($scope, $http) {
-    $http.get("http://localhost:8080/tweets?page=1").success(function (data) {
+    $http.get("http://localhost:5000/tweets?page=1").success(function (data) {
     	$scope.tweets = data;
     	console.log($scope.tweets.length);
     	var page = 2;
@@ -11,7 +11,7 @@ app.controller('tweets', ['$scope', '$http', function ($scope, $http) {
 				return;
 			}
 			$scope.busy = true;
-			$http.get("http://localhost:8080/tweets?page="+page).success(function (data) {
+			$http.get("http://localhost:5000/tweets?page="+page).success(function (data) {
 				for(var i = 0;i<data.length;i++){
 					$scope.tweets.push(data[i]);
 				}
@@ -23,7 +23,7 @@ app.controller('tweets', ['$scope', '$http', function ($scope, $http) {
 }]);
 
 app.controller('user',['$scope','$http', function ($scope,$http) {
-	$http.get("http://localhost:8080/info-user").success(function (data) {
+	$http.get("http://localhost:5000/info-user").success(function (data) {
 		$scope.infos = data;
 	});
 }]);
@@ -31,13 +31,13 @@ app.controller('user',['$scope','$http', function ($scope,$http) {
 
 
 app.controller('trends',['$scope','$http',function ($scope,$http) {
-	$http.get("http://localhost:8080/trends").success(function (data) {
+	$http.get("http://localhost:5000/trends").success(function (data) {
 		$scope.trends = data;
 	});
 }]);
 
 app.controller('tweets-media', ['$scope', '$http', function ($scope, $http) {
-    $http.get("http://localhost:8080/tweets-media?page=1").success(function (data) {
+    $http.get("http://localhost:5000/tweets-media?page=1").success(function (data) {
     	$scope.tweets = data;
     	console.log($scope.tweets.length);
     	var page = 2;
@@ -47,7 +47,7 @@ app.controller('tweets-media', ['$scope', '$http', function ($scope, $http) {
 				return;
 			}
 			$scope.busy = true;
-			$http.get("http://localhost:8080/tweets-media?page="+page).success(function (data) {
+			$http.get("http://localhost:5000/tweets-media?page="+page).success(function (data) {
 				for(var i = 0;i<data.length;i++){
 					$scope.tweets.push(data[i]);
 				}
---
+++ server-flask.py
@@ -0,0 +1,102 @@
+from flask import Flask,request, Response
+from flask_cors import CORS, cross_origin
+import os
+import tweepy
+import demjson
+import datetime
+
+
+app = Flask(__name__)
+CORS(app)
+
+#inicia variaveis de acesso twitter
+cons_key = "OvfdRuq1u3iMfuYwOmJvaauad"
+cons_secret = "aMrb4y7gyDwiLzeFzgUSh3af5PgYBe6Wh2oasih2AhVRFsqG0s"
+access_token = "1864929535-UjtQEsaEeY7LiNao48jHKpLPqQnSfjldc766G2A"
+access_token_secret = "HgIpDHq3b1hvkFA0PojzrrY0y3KSQvST0qYurPiUuBZJw"
+
+
+
+#cria objeto para manipular a api do twitter
+auth = tweepy.OAuthHandler(cons_key, cons_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+@app.route('/tweets')
+def tweets():
+	tweets = api.user_timeline(id="americanascom",count=5,page=request.args.get('page'))
+	imageTweet = ""
+	json = []
+	for t in tweets:
+		if 'media' in t.entities:
+			for image in  t.entities['media']:
+				imageTweet = image['media_url']
+		json.append({
+			'text': t.text,
+			'img': imageTweet,
+			'imgProfile': t.user.profile_image_url,
+			'name': t.user.name,
+			'screen_name': "@"+t.user.screen_name,
+			'replyTo': t.in_reply_to_screen_name,
+			
+		})
+	
+	tweet = demjson.encode(json)
+	return Response(tweet,mimetype = 'application/json')
+
+#get tweets with media
+@app.route('/tweets-media')
+def tweetsImage():
+	tweets = api.user_timeline(id="americanascom",count=5,page=request.args.get('page'))
+	json = []
+	for t in tweets:
+		if 'media' in t.entities:
+			for image in  t.entities['media']:
+				imageTweet = image['media_url']
+				json.append({
+					'text': t.text,
+					'img': imageTweet,
+					'imgProfile': t.user.profile_image_url,
+					'name': t.user.name,
+					'screen_name': "@"+t.user.screen_name,
+					'replyTo': t.in_reply_to_screen_name,
+				})
+	tweet = demjson.encode(json)
+	return Response(tweet,mimetype = 'application/json')
+
+@app.route("/info-user")
+def info():
+	info = api.get_user(id="americanascom")
+	json = {
+		"name": info.name, 
+		"description": info.description,
+		"location": info.location,
+		"followers_count": info.followers_count,
+		"friends_count": info.friends_count,
+		"likes": info.favourites_count,
+		"month": info.created_at.strftime("%B"),
+		"year": info.created_at.year,
+		"tweets": info.statuses_count,
+		"screen_name": "@"+info.screen_name,
+		"img_background": info.profile_banner_url,
+		"profile_image_url": info.profile_image_url
+	}
+	infoUser = demjson.encode(json)
+	return Response(infoUser,mimetype = 'application/json')
+
+@app.route("/trends")
+def trend():
+	json = []
+	trends = api.trends_place(1)
+	data = trends[0]
+	trends = data['trends']
+	for t in range(10):
+		json.append({
+			"name": trends[t]['name'],
+			"tweet_count": trends[t]['tweet_volume'] 
+		})
+
+	infoTrends = demjson.encode(json)
+	return Response(infoTrends,mimetype = 'application/json')
+
+app.run()
